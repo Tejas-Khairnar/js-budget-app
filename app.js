c68a6cc9b796c4593375bca73cbe3bf9352ba9d9
Tejas-Khairnar <ktejas6892@gmail.com>
2020-06-08 11:33:28 +0530

    Added initialization function in app

--- app.js
+++ app.js
@@ -58,14 +58,32 @@ var UIController = (function () {
  * @param UICtrl - refer to UIController
  */
 var controller = (function (budgetCtrl, UICtrl) {
-  // get DOM strings from UIController
-  var DOM = UICtrl.getDOMStrings();
+  // function containing all event listener
+  var setupEventListener = function () {
+    // get DOM strings from UIController
+    var DOM = UICtrl.getDOMStrings();
+
+    // event listener for add button by using css selector
+    document
+      .querySelector(DOM.inputButton)
+      .addEventListener('click', ctrlAddItem);
+
+    // keypress event not specific to any element but for overall document
+    /**
+     * @param event - automatically passed by browser when event happens, continuously in this case
+     */
+    document.addEventListener('keypress', function (event) {
+      // if ENTER is pressed, keyCode => newer browser, which => older browser
+      if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  };
 
   // shared method called in both event listener below
   var ctrlAddItem = function () {
     // get field input data, called function from UIController
     var input = UICtrl.getInput();
-    console.log(input);
 
     // add item to budget controller
     // add new item to UI
@@ -73,19 +91,13 @@ var controller = (function (budgetCtrl, UICtrl) {
     // display budget on UI
   };
 
-  // event listener for add button by using css selector
-  document
-    .querySelector(DOM.inputButton)
-    .addEventListener('click', ctrlAddItem);
-
-  // keypress event not specific to any element but for overall document
-  /**
-   * @param event - automatically passed by browser when event happens, continuously in this case
-   */
-  document.addEventListener('keypress', function (event) {
-    // if ENTER is pressed, keyCode => newer browser, which => older browser
-    if (event.keyCode === 13 || event.which === 13) {
-      ctrlAddItem();
-    }
-  });
+  // public initialization function, always return as object outside IIFE
+  return {
+    init: function () {
+      setupEventListener();
+    },
+  };
 })(budgetController, UIController);
+
+// initialization of budget app
+controller.init();
