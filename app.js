2fbac4f50096515a4572e33820b8d150b545f5eb
Tejas-Khairnar <ktejas6892@gmail.com>
2020-06-08 10:36:16 +0530

    Added first event listener

--- app.js
+++ app.js
@@ -26,6 +26,31 @@ var budgetController = (function () {})();
 var UIController = (function () {})();
 
 // budget app controller module, communicationg with budgetController as well as UIController by passing them as argument to it
+/**
+ * @param budgetCtrl - refer to budgetController
+ * @param UICtrl - refer to UIController
+ */
 var controller = (function (budgetCtrl, UICtrl) {
-  // some code here
+  // shared method called in both event listener below
+  var ctrlAddItem = function () {
+    // get field input data
+    // add item to budget controller
+    // add new item to UI
+    // calculate budget
+    // display budget on UI
+  };
+
+  // event listener for add button by using css selector
+  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+  // keypress event not specific to any element but for overall document
+  /**
+   * @param event - automatically passed by browser when event happens, continuously in this case
+   */
+  document.addEventListener('keypress', function (event) {
+    // if ENTER is pressed, keyCode => newer browser, which => older browser
+    if (event.keyCode === 13 || event.which === 13) {
+      ctrlAddItem();
+    }
+  });
 })(budgetController, UIController);
